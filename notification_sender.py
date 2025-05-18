ce74a66b6ff89570b94c35ab39d14654dfb5008d
ANJALI606-707 <2205792@kiit.ac.in>
2025-05-19 01:21:36 +0530

    Delete notification_sender.py

--- notification_sender.py
+++
@@ -1,6 +0,0 @@
-from models import Notification
-
-# Simulate sending to a queue (RabbitMQ/Kafka simulation)
-def send_notification_to_queue(notification: Notification):
-    # In real-world, push to RabbitMQ or Kafka here
-    print(f"Pushed to queue: {notification}")
